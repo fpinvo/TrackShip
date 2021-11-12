59beacd58fdab0bedd4268578a1e7cd13dd44b39
Farhan Pirzada <Farhan.Pirzada@invozone.com>
2021-11-12 07:50:47 +0500

    first commit

---
+++ TackerShip2.py
@@ -0,0 +1,55 @@
+
+
+from pykafka import KafkaClient
+import json
+from datetime import datetime
+import uuid
+import time
+
+'''
+READ COORDINATES FROM GEOJSON
+'''
+
+input_file = open('./data/Ship2.json')
+json_array = json.load(input_file)
+coordinates = json_array['features'][0]['geometry']['coordinates']
+
+#GENERATE UUID
+def generate_uuid():
+    return uuid.uuid4()
+
+
+'''
+KAFKA PRODUCER
+'''
+
+client = KafkaClient(hosts="localhost:9092")
+topic = client.topics['testTrakership']
+producer = topic.get_sync_producer()
+
+'''
+CONSTRUCT MESSAGE AND SEND IT TO KAFKA
+'''
+data = {}
+data['Shipline'] = '00002'
+
+def generate_checkpoint(coordinates):
+    i = 0
+    while i < len(coordinates):
+        data['key'] = data['Shipline'] + '_' + str(generate_uuid())
+        data['timestamp'] = str(datetime.utcnow())
+        data['latitude'] = coordinates[i][1]
+        data['longitude'] = coordinates[i][0]
+        message = json.dumps(data)
+        print(message)
+        producer.produce(message.encode('ascii'))
+        time.sleep(1)
+
+        #if Ship reaches last coordinate, start from beginning
+        if i == len(coordinates)-1:
+            i = 0
+        else:
+            i += 1
+
+generate_checkpoint(coordinates)
+
---
+++ static/leaf.js
@@ -0,0 +1,45 @@
+var mymap = L.map('mapid').setView([24.427,60.996], 6);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZnBpbnZvIiwiYSI6ImNrdnZoajQzOGJ4aGUyb3M3YWE0Yjg1aHQifQ.IIQklQtrUJnooUrXcFoOgg' //ENTER YOUR ACCESS TOKEN HERE
+}).addTo(mymap);
+
+mapMarkers1 = [];
+mapMarkers2 = [];
+mapMarkers3 = [];
+
+var source = new EventSource('/topic/testTrakership'); //ENTER YOUR TOPICNAME HERE
+source.addEventListener('message', function(e){
+
+  console.log('Message');
+  obj = JSON.parse(e.data);
+  console.log(obj);
+
+  if(obj.Shipline == '00001') {
+    for (var i = 0; i < mapMarkers1.length; i++) {
+      mymap.removeLayer(mapMarkers1[i]);
+    }
+    marker1 = L.marker([obj.latitude, obj.longitude]).addTo(mymap);
+    mapMarkers1.push(marker1);
+  }
+
+  if(obj.Shipline == '00002') {
+    for (var i = 0; i < mapMarkers2.length; i++) {
+      mymap.removeLayer(mapMarkers2[i]);
+    }
+    marker2 = L.marker([obj.latitude, obj.longitude]).addTo(mymap);
+    mapMarkers2.push(marker2);
+  }
+
+  if(obj.Shipline == '00003') {
+    for (var i = 0; i < mapMarkers3.length; i++) {
+      mymap.removeLayer(mapMarkers3[i]);
+    }
+    marker3 = L.marker([obj.latitude, obj.longitude]).addTo(mymap);
+    mapMarkers3.push(marker3);
+  }
+}, false);
